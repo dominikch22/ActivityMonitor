e8435f61add44bf264de414d4a155da824b4e0d4
qwert qwert <Dominikch19@gmail.com>
2024-01-21 15:18:39 +0100

    added inupt calendar, active program list, domains history list and charts

--- ActivityMonitor/MainWindow.xaml.cs
+++ ActivityMonitor/MainWindow.xaml.cs
@@ -35,6 +35,7 @@ namespace ActivityMonitor
         public MainWindow()
         {
             InitializeComponent();
+            WindowState = WindowState.Maximized;
 
             InputData = new InputData();
 
@@ -43,25 +44,113 @@ namespace ActivityMonitor
             thread.Start();
 
 
+            CreateCalender();
+
+            keyPressedChart.Series[0].Points.Clear();
+            mouseClickChart.Series[0].Points.Clear();
+            mouseDistanceChart.Series[0].Points.Clear();
+            for (int i = 0; i < InputData.MouseMoves.Length; i++)
+            {
+                keyPressedChart.Series[0].Points.Add(InputData.MouseMoves[i] / 1000, i).AxisLabel = (i / 60).ToString() + ":" + (i % 60).ToString();
+                mouseClickChart.Series[0].Points.Add(InputData.MouseMoves[i] / 1000, i).AxisLabel = (i / 60).ToString() + ":" + (i % 60).ToString();
+                mouseDistanceChart.Series[0].Points.Add(InputData.MouseMoves[i] / 1000, i).AxisLabel = (i / 60).ToString() + ":" + (i % 60).ToString();
 
 
+            }
 
-          
+            addEleentsToListBoxes();
 
+        }
 
-
+        private void addEleentsToListBoxes() {
+            for (int i = 1; i <= 100; i++)
+            {
+                ActiveProgramsList.Items.Add($"Item {i}");
+                DomainsHistory.Items.Add($"Item {i}");
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             lock (InputData.LockObject)
             {
-                chart1.Series[0].Points.Clear();
+                keyPressedChart.Series[0].Points.Clear();
+                mouseClickChart.Series[0].Points.Clear();
+                mouseDistanceChart.Series[0].Points.Clear();
                 for (int i = 0; i < InputData.MouseMoves.Length; i++)
                 {
-                    chart1.Series[0].Points.Add(InputData.MouseMoves[i] / 1000, i).AxisLabel = (i/60).ToString()+":"+(i%60).ToString();
+                    keyPressedChart.Series[0].Points.Add(InputData.MouseMoves[i] / 1000, i).AxisLabel = (i / 60).ToString() + ":" + (i % 60).ToString();
+                    mouseClickChart.Series[0].Points.Add(InputData.MouseMoves[i] / 1000, i).AxisLabel = (i / 60).ToString() + ":" + (i % 60).ToString();
+                    mouseDistanceChart.Series[0].Points.Add(InputData.MouseMoves[i] / 1000, i).AxisLabel = (i / 60).ToString() + ":" + (i % 60).ToString();
+
 
                 }
+            }
+        }
+
+        private void CreateCalender() {
+            System.Windows.Controls.Grid calenderGrid = (System.Windows.Controls.Grid)this.FindName("CalenderGrid");
+            List<string> dates = GenerateDates(60);
+            int count = 59;
+            for (int row = 0; row < 6; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    Button button = new Button
+                    {
+                        Content = $" {row * 10 + col + 1}",
+                        Margin = new Thickness(2),
+                        Width = 25,
+                        Height = 25,
+                        Background = Brushes.White,
+                        ToolTip = dates[count],
+                        //HorizontalAlignment = HorizontalAlignment.Stretch,
+                        //VerticalAlignment = VerticalAlignment.Stretch
+                    };
+                    count--;
+
+                    System.Windows.Controls.Grid.SetRow(button, row);
+                    System.Windows.Controls.Grid.SetColumn(button, col);
+
+                    // Attach an event handler if needed
+                    button.Click += Button_Click;
+
+                    // Add the button to the grid
+                    calenderGrid.Children.Add(button);
+                }
+            }
+        }
+
+        static List<string> GenerateDates(int numberOfDays)
+        {
+            List<string> dates = new List<string>();
+
+            DateTime currentDate = DateTime.Now;
+
+            for (int i = 0; i < numberOfDays; i++)
+            {
+                dates.Add(currentDate.ToString("dd-MM-yyyy"));
+                currentDate = currentDate.AddDays(-1);
+            }
+
+            return dates;
+        }
+
+        private void DomainsHistoryList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            // Handle the selection change event here
+            if (DomainsHistory.SelectedItem != null)
+            {
+                MessageBox.Show($"You clicked on: {DomainsHistory.SelectedItem.ToString()}");
+            }
+        }
+
+        private void ActiveProgramsList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            // Handle the selection change event here
+            if (ActiveProgramsList.SelectedItem != null)
+            {
+                MessageBox.Show($"You clicked on: {ActiveProgramsList.SelectedItem.ToString()}");
             }
         }
 
